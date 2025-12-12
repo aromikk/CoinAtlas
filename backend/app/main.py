a643d56505aa39d298f70a5b2d465badeebfa0e0
Аронов Михаил <aronovmihail@MacBook-Pro-Sigma.local>
2025-12-12 18:02:17 +0300

    Добавили пагенацию, запрос на число страниц

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01a7151dde01afbd714a65b8f14164963dfd8b1093c40eb40572e449d9d13b9c
-size 16524
+oid sha256:cda00572bb4e74a2727080655828a0e5955ba7642346f6c7e98acdff96af7c9c
+size 17071
